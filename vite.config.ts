98e385cd2127c5790bf9f8ebcaed5f716aebad0b
Chris Bellman <52683037+TheChrisBellman@users.noreply.github.com>
2026-08-28 18:02:59 -0400

    Fix GitHub Pages asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 const repositoryName = process.env.GITHUB_REPOSITORY?.split('/').pop()
 
 export default defineConfig({
-  base: repositoryName ? `/${repositoryName}/` : '/',
+  base: repositoryName ? `/${repositoryName}/` : './',
   plugins: [react()],
 })
